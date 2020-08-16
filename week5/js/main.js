5b5e8f908bb3227bc6b3f7a07e57a0bab15665e1
karen <miskaren25@gmail.com>
2020-08-16 21:41:30 +0800

    產品列表
    -產品列表顯示api串接
    -單一產品細節api串接

--- week5/js/main.js
+++ week5/js/main.js
@@ -24,6 +24,7 @@ new Vue({
 
     // Vue 有雙向綁定的特性，在此需要先定義基本的資料才能進行綁定
     data: {
+        products: [],
         form: {
             name: '',
             email: '',
@@ -32,10 +33,51 @@ new Vue({
             payment: '',
             message: '',
         }, // 自訂的資料名稱
+        productMore: {},
+        api: {
+            token: '',
+            path: 'https://course-ec-api.hexschool.io/api/',
+            uuid: '622aca82-f046-487a-a10e-aa00aabcc682',
+        },
+    },
+    created() {
+        this.api.token = document.cookie.replace(/(?:(?:^|.*;\s*)karentoken\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+        //預設帶入 token
+        axios.defaults.headers.common.Authorization = `Bearer ${this.api.token}`;
+
+        if (this.api.token == '') {
+            window.location = 'productPage.html';
+        } else {
+            this.getProducts(1);
+        }
     },
     methods: {
         submitForm() {
             console.log('送出表單');
+        },
+        getProducts(page = 1) {
+            console.log('getProducts');
+            const url = `${this.api.path}${this.api.uuid}/ec/products?page=${page}`;
+            this.isLoading = true;
+            axios
+                .get(url)
+                .then(response => {
+                    console.log('get products complete');
+                    this.products = response.data.data;
+                    this.isLoading = false;
+                })
+                .catch(error => {
+                    this.isLoading = false;
+                });
+        },
+        getMore(id) {
+            const url = `${this.api.path}${this.api.uuid}/ec/product/${id}`;
+            axios.get(url).then(response => {
+                this.productMore = response.data.data;
+
+                $('#productModal').modal('show');
+                console.log('get productdetails complete');
+            });
         },
     },
 });
